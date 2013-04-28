f439aa43ab664b36bb3b6171a5d3f730737c4d0b
Muhammad Wajahat <wajahat.lums@gmail.com>
2013-04-28 17:45:12 +0500

    CSS Changed
    Added some images for team members, changed the color of menu
    background, added profile page

---
+++ Source Code/profile.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by Free CSS Templates
+http://www.freecsstemplates.org
+Released for free under a Creative Commons Attribution 2.5 License
+
+Name       : Sovereign 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20120902
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Profile</title>
+<LINK REL=ICON HREF="images/icon.png">
+<link href='http://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
+
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+
+</head>
+<body>
+<div id="wrapper">
+  <div id="header-wrapper">
+		<div id="header" class="container">
+		  <div id="logo">
+				<h1>SarSubz LUMS</h1>
+			</div>
+			<div id="menu">
+			  <ul>
+					<li ><a href="home.php">Home</a></li>
+					<li><a href="credits.html">Credits</a></li>
+					<li><a href="logout.php">Logout</a></li>
+			  </ul>
+			</div>
+		</div>
+	</div>
+	<div id="page">
+		
+		<div class="post">
+			<h2 class="title"><a href="#">Welcome To SarSubz Car Pooling System</a></h2>
+			<p class="meta"><span class="date"><?php print(Date("l F d, Y"));?></span>
+			<br/>
+	
+			<br/>
+		</div>
+		
+		<div class="content">
+			<div class="info">
+				<table>
+					<tr> </tr>
+				</table>
+			<!--Fetch Data from the database -->
+			</div>
+			<div class="update">
+				<form>
+					
+				</form>
+			</div>
+			<br class="clear" />
+		</div>
+		
+	  
+		<!-- end #content --><!-- end #sidebar -->
+		<div style="clear: both;">&nbsp;</div>
+  </div>
+	<div class="container"><img src="images/img03.png" width="1000" height="40" alt="" /></div>
+	</div>
+	<!-- end #header -->
+
+	
+	<!-- end #page --> 
+</div>
+<div id="footer-content"></div>
+<div id="footer">
+	<p>Copyright (c) 2012 Sitename.com. All rights reserved. Design by <a href="http://www.freecsstemplates.org">FCT</a>. Photos by <a href="http://fotogrph.com/">Fotogrph</a>.</p>
+</div>
+<!-- end #footer -->
+</body>
+</html>
