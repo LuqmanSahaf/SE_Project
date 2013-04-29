4601268da75bdb4e1219f5d20b253e38b9907a17
Muhammad Wajahat <wajahat.lums@gmail.com>
2013-04-29 23:28:53 +0500

    Added Tabs in searchLift, blank templates for modifyLift and lifts Page

--- Source Code/modifylift.php
+++ Source Code/modifylift.php
@@ -15,7 +15,7 @@ Released   : 20120902
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>Lifts</title>
+<title>Modify Lift</title>
 <LINK REL=ICON HREF="images/icon.png">
 <link href='http://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
@@ -58,8 +58,10 @@ Released   : 20120902
 	</div>
 	<div id="page">
 		
-		<div class="content">
 		
+		<div class="content">
+
+			<br class="clear" />
 		</div>
 		
 	  
